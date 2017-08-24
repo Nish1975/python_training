325889eefee5902150c7e1d940c342db58417ba6
Nish1975 <zikzik8@msn.com>
2017-08-24 16:02:57 +0300

    проверка валидности фикстуры и проверка состояния сессии

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -1,6 +1,5 @@
 
 def test_mod_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.mod_first_contact()
-    app.session.logout()
+
 
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,6 +1,5 @@
 
 def test_mod_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.mod_first_group()
-    app.session.logout()
+
 
