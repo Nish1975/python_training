c677c1b6e7f828c10f107d91a643421bfd0fa0fb
Nish1975 <zikzik8@msn.com>
2017-08-28 19:17:49 +0300

    проверки для всех полей контакта на главной странице

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 class ContactHelper:
 
@@ -194,7 +195,60 @@ class ContactHelper:
                 name1 = strs[2].text
                 name2 = strs[1].text
                 id=element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(firstname=name1, lastname=name2, id=id))
+                all_phones=strs[5].text
+                all_emails=strs[4].text
+                self.contact_cache.append(Contact(firstname=name1, lastname=name2, id=id,
+                                                  all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
+    def open_contact_to_edit_by_index(self,index):
+        wd = self.app.wd
+        self.open_contacts_page()
+        row=wd.find_elements_by_name("entry")[index]
+        cell=row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
 
+    def open_contact_view_by_index(self,index):
+        wd = self.app.wd
+        self.open_contacts_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+
+    def get_contact_info_from_edit_page(self,index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname= wd.find_element_by_name("firstname").get_attribute("value")
+        lastname= wd.find_element_by_name("lastname").get_attribute("value")
+        #middlename = wd.find_element_by_name("middlename").get_attribute("value")
+        #title = wd.find_element_by_name("title").get_attribute("value")
+        #nickname = wd.find_element_by_name("nickname").get_attribute("value")
+        #company= wd.find_element_by_name("company").get_attribute("value")
+        #address = wd.find_element_by_name("address").get_attribute("value")
+        id= wd.find_element_by_name("id").get_attribute("value")
+        homephone= wd.find_element_by_name("home").get_attribute("value")
+        workphone= wd.find_element_by_name("work").get_attribute("value")
+        mobilephone= wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone= wd.find_element_by_name("phone2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+        #ayear= wd.find_element_by_name("ayear").get_attribute("value")
+        #byear= wd.find_element_by_name("byear").get_attribute("value")
+        #homepage= wd.find_element_by_name("homepage").get_attribute("value")
+        #address2= wd.find_element_by_name("address2").get_attribute("value")
+        #phone2= wd.find_element_by_name("phone2").get_attribute("value")
+        #notes= wd.find_element_by_name("notes").get_attribute("value")
+        return Contact(firstname=firstname,lastname=lastname,id=id,home=homephone,work=workphone,
+                       mobile=mobilephone,phone2=secondaryphone, email=email,email2=email2, email3=email3)
+
+    def get_contact_from_view_page(self,index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text=wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(home=homephone, work=workphone, mobile=mobilephone, phone2=secondaryphone)
