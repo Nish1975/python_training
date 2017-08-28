cb04dda30e34e8e0b36d38db2f731c7e50dc3c49
Nish1975 <zikzik8@msn.com>
2017-08-28 14:17:23 +0300

    кеширование при загрузке списков групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -87,6 +87,7 @@ class ContactHelper:
         wd.find_element_by_name("notes").send_keys(Contact.notes)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.contact_cache = None
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -94,6 +95,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def mod_first_contact(self, Contact):
         wd = self.app.wd
@@ -166,22 +168,26 @@ class ContactHelper:
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(Contact.notes)
         wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
+        self.contact_cache = None
 
     def count_contact(self):
         wd = self.app.wd
         self.open_contacts_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        self.open_contacts_page()
-        contacts=[]
-        for element in wd.find_elements_by_name("entry"):
-            strs = element.find_elements_by_tag_name("td")
-            name1 = strs[2].text
-            name2 = strs[1].text
-            id=element.find_element_by_name("selected[]").get_attribute("value")
-            contacts.append(Contact(firstname=name1, lastname=name2, id=id))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.open_contacts_page()
+            self.contact_cache=[]
+            for element in wd.find_elements_by_name("entry"):
+                strs = element.find_elements_by_tag_name("td")
+                name1 = strs[2].text
+                name2 = strs[1].text
+                id=element.find_element_by_name("selected[]").get_attribute("value")
+                self.contact_cache.append(Contact(firstname=name1, lastname=name2, id=id))
+        return list(self.contact_cache)
 
 
